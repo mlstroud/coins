efad78989964c186812289233ebda116f81681bc
Matt Stroud <lixingke06@gmail.com>
2020-05-12 13:53:44 -0700

    add code to set change

--- Coins/Models/Coins.cs
+++ Coins/Models/Coins.cs
@@ -3,6 +3,15 @@ namespace Coins.Models
 {
   public class Coin
   {
+    public static double Change { get; set; }
+    public static int Quarters { get; set; }
+    public static int Dimes { get; set; }
+    public static int Nickels { get; set; }
+    public static int Pennies { get; set; }
 
+    public static void SetChange(double change)
+    {
+      Change = change;
+    }
   }
 }
