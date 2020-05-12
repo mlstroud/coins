9bc87a6e006b01c1eb7a46aaab58ea0ebfe126c4
Matt Stroud <lixingke06@gmail.com>
2020-05-12 14:23:27 -0700

    add method for getting input from user

--- Coins/Program.cs
+++ Coins/Program.cs
@@ -7,7 +7,34 @@ namespace Coins
   {
     public static void Main()
     {
+      PromptUser();
+    }
 
+    public static void PromptUser()
+    {
+      Console.WriteLine("Enter an amount in change to see the breakdown:");
+      Console.WriteLine("-----------------------------------------------");
+      GetChange();
+    }
+
+    public static void GetChange()
+    {
+      bool parsed = false;
+
+      while (!parsed)
+      {
+        string userInput = Console.ReadLine();
+        if (Double.TryParse(userInput, out double change))
+        {
+          Coin.Change = change;
+          parsed = true;
+        }
+        else
+        {
+          Console.WriteLine("Sorry, invalid input. Try again:");
+
+        }
+      }
     }
   }
 }
