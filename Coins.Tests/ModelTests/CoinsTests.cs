13fb4fd7f66f773ea996051ee66d3faf58c03a42
Matt Stroud <lixingke06@gmail.com>
2020-05-12 13:53:16 -0700

    add test for setting change amount

--- Coins.Tests/ModelTests/CoinsTests.cs
+++ Coins.Tests/ModelTests/CoinsTests.cs
@@ -6,6 +6,15 @@ namespace Coins.Tests
   [TestClass]
   public class CoinTests
   {
+    [TestMethod]
+    public void SetChange_SetsChange_Double()
+    {
+      double change = 1.43;
+      Coin.SetChange(1.43);
 
+      double result = Coin.Change;
+
+      Assert.AreEqual(change, result);
+    }
   }
 }
